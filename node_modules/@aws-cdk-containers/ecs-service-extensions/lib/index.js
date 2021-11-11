3118aed9ab92bf16b4d7b0f7aca43319c164b9af
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2021-11-11 16:34:57 -0600

    adding node_modules

---
+++ node_modules/@aws-cdk-containers/ecs-service-extensions/lib/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./service"), exports);
+__exportStar(require("./service-description"), exports);
+__exportStar(require("./environment"), exports);
+__exportStar(require("./extensions"), exports);
+__exportStar(require("./extensions/extension-interfaces"), exports);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7QUFBQSw0Q0FBMEI7QUFDMUIsd0RBQXNDO0FBQ3RDLGdEQUE4QjtBQUM5QiwrQ0FBNkI7QUFDN0Isb0VBQWtEIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnLi9zZXJ2aWNlJztcbmV4cG9ydCAqIGZyb20gJy4vc2VydmljZS1kZXNjcmlwdGlvbic7XG5leHBvcnQgKiBmcm9tICcuL2Vudmlyb25tZW50JztcbmV4cG9ydCAqIGZyb20gJy4vZXh0ZW5zaW9ucyc7XG5leHBvcnQgKiBmcm9tICcuL2V4dGVuc2lvbnMvZXh0ZW5zaW9uLWludGVyZmFjZXMnOyJdfQ==
